e0bf840b96c5553838537a0a1d5bdeb251954c9e
Innocent <99240481+Asabuodu@users.noreply.github.com>
2025-01-21 15:17:34 +0100

    backend functions added

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,9 +1,14 @@
 // src/auth/dto/signin.dto.ts
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class SigninDto {
   @IsEmail()
   email: string;
+
+
+   @IsString()
+   @MinLength(5)
+   username: string;
 
   @IsString()
   password: string;
